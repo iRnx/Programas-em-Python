6b64f4b68db07f44e1ce94ce2bc12c5dc4c31de0
Renan <86449025+iRnx@users.noreply.github.com>
2021-07-27 13:03:49 -0300

    Média Aritmética

---
+++ Ex007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média. #
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1 + n2) / 2
+print(f'A Média entre {n1} e {n2} é igual a {media}')
