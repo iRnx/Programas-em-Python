2252bc924c374811167fe7918eee5e3e96266b47
Renan <86449025+iRnx@users.noreply.github.com>
2021-07-27 12:57:35 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ Ex006.py
@@ -0,0 +1,9 @@
+# Crie um algoritmo que leia um número e mostre o seu Dobro, Triplo e raiz quadrada. #
+
+a = int(input('Digite um número: '))
+dobro = a * 2
+triplo = a * 3
+raiz = a ** (1/2)
+print(f'O Dobro de {a} vale {dobro}')
+print(f'O Triplo de {a} vale {triplo}')
+print(f'A Raiz Quadrada de {a} é igual a {raiz}')
