9b4181ae14852f29e7957ad5a07b73fe99ce8928
Renan <86449025+iRnx@users.noreply.github.com>
2021-07-27 12:10:48 -0300

    Somando dois Números

---
+++ Ex003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre elas. #
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print(f'A Soma de {n1} + {n2} = {soma}')
