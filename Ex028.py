8fc221944ed784ecfbdf34617af7c3de5efeb5ab
Renan <86449025+iRnx@users.noreply.github.com>
2021-09-03 15:55:18 -0300

    Jogo da Adivinhação

---
+++ Ex028.py
@@ -0,0 +1,20 @@
+"""
+Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário
+tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o
+usuário venceu ou perdeu.
+"""
+
+from random import randint
+from time import sleep
+pc = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que numero eu pensei? '))
+print('PROCESSANDO...')
+sleep(2)
+if jogador == pc:
+    print('PARABENS! Você conseguiu me vencer!')
+else:
+    print(f'GANHEI! Eu pensei no número {pc} e não no {jogador}')
+
