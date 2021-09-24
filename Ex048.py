458bd4b6931df90f15e16e9d57f9f4193f228028
Renan <86449025+iRnx@users.noreply.github.com>
2021-09-23 23:19:00 -0300

    Soma ímpares Múltiplos de três

---
+++ Ex048.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram
+no intervalo de 1 até 500.
+"""
+
+cont = 0
+soma = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1  # Contador #
+        soma = soma + c  # Acumulador #
+print(f'A soma de todos os {cont} valores solicitados é {soma}')
