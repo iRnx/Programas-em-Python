4d840fa572fd0968307246961ce9605ccc9f9f7f
Renan <86449025+iRnx@users.noreply.github.com>
2021-09-30 16:26:20 -0300

    Detector de Palíndromo

---
+++ Ex053.py
@@ -0,0 +1,32 @@
+"""
+Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+"""
+
+# PRIMEIRA FORMA#
+
+# Criar um palíndromo usando for #
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso = inverso + junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+
+# SEGUNDA FORMA #
+
+# Criar um palíndromo fazendo fatiamento. #
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
