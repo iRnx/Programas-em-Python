6664a474028535c70f3b3b44971e64c7fd8a6e9b
Renan <86449025+iRnx@users.noreply.github.com>
2021-11-30 07:41:01 -0300

    Simulador de caixa Eletrônico

---
+++ Ex071.py
@@ -0,0 +1,23 @@
+print('=' * 30)
+print('          BANCO RNX              ')
+print('=' * 30)
+valor = int(input('Que valor vc quer sacar? '))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total = total - ced
+        totced = totced + 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
